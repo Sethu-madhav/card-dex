74a536dd5bb719124b3ab4293a36263db8423ebe
Sethu-madhav <sethu4784@gmail.com>
2023-07-19 18:14:59 -0400

    Initializing state in class component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from "./logo.svg";
+import React from "react";
 import { Component } from "react";
+import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component() {
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Joe",
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Play with React
-          </a>
+          <img src={logo} alt="logo" className="App-logo" />
+          <p>Hi {this.state.name}.</p>
+          <button>Change name</button>
         </header>
       </div>
     );
